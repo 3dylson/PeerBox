b06deab7d1e4d809bf4e02ceba11d91d72ee72e7
Edylson Manuel Gonçalves Frederico <edylson_frederico@outlook.com>
2021-04-22 00:10:31 +0100

    Jgroups connection

--- src/main/java/pt/ipb/dsys/peerbox/Main.java
+++ src/main/java/pt/ipb/dsys/peerbox/Main.java
@@ -1,15 +1,32 @@
 package pt.ipb.dsys.peerbox;
 
+import org.jgroups.JChannel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import pt.ipb.dsys.peerbox.jgroups.DefaultProtocols;
+import pt.ipb.dsys.peerbox.jgroups.LoggingReceiver;
 
 public class Main {
 
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
+    public static final String CLUSTER_NAME = "PeerBox";
 
     public static void main(String[] args) {
-        // Please start here...
-        logger.info("Main (dummy) started.");
+
+        // Cluster initialization and connection
+        try (JChannel channel = new JChannel(DefaultProtocols.gossipRouter())) {
+
+            channel.connect(CLUSTER_NAME);
+            channel.setReceiver(new LoggingReceiver());
+            channel.setDiscardOwnMessages(true);  // <-- Own messages will be discarded
+
+            String hostname = DnsHelper.getHostName();
+
+
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+        }
+
     }
 
 }
