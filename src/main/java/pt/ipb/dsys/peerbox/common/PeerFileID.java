f95ea9f6c9536e013700c666349fe0d0883a5966
Edylson Manuel Gonçalves Frederico <edylson_frederico@outlook.com>
2021-04-29 04:45:47 +0100

    thinking of PeerFileID as a message header

--- src/main/java/pt/ipb/dsys/peerbox/common/PeerFileID.java
+++ src/main/java/pt/ipb/dsys/peerbox/common/PeerFileID.java
@@ -1,81 +1,81 @@
 package pt.ipb.dsys.peerbox.common;
 
-import org.jgroups.util.UUID;
+import org.jgroups.Global;
+import org.jgroups.Header;
+import org.jgroups.util.Util;
 
-import java.io.File;
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
 import java.io.Serializable;
+import java.util.function.Supplier;
 
-public class PeerFileID implements Serializable, Comparable<PeerFileID> {
+public class PeerFileID extends Header implements Serializable {
 
     // Members will depend on the metadata specific to your implementation
 
     public static final long serialVersionUID = 1L;
 
-    UUID ID;
-    String name;
-    String path;
-    long timestamp;
-    //String filehash;
+    protected static final short ID=3500;
 
-    public UUID getID() {
+
+    protected String  filename;
+    protected boolean eof;
+
+    public PeerFileID() {} // for de-serialization
+
+    public PeerFileID(String filename, boolean eof) {
+        this.filename=filename;
+        this.eof=eof;
+    }
+
+
+    /**
+     * Returns the magic-ID. If defined in jg-magic-map.xml, the IDs need to be the same
+     */
+    @Override
+    public short getMagicId() {
         return ID;
     }
 
-    public void setID(UUID ID) {
-        this.ID = ID;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPath() {
-        return path;
-    }
-
-    public void setPath(String path) {
-        this.path = path;
-    }
-
-    public long getTimestamp() {
-        return timestamp;
-    }
-
-    public void setTimestamp(long timestamp) {
-        this.timestamp = timestamp;
-    }
-
-
-    public PeerFileID() {
-
-            //System.out.println(path + "\\" + name);
-            this.ID = UUID.randomUUID();
-            this.timestamp = System.currentTimeMillis();
-            //this.setFilehash(Integer.toString(this.getName().hashCode() + this.getPath().hashCode()));
-
-    }
-
-    /*
-    * Compares filesID to see which one has the earliest timestamp
-    * If the timestamps can't be compared, compares the IDs
-    *
-    * */
+    /**
+     * Creates an instance of the class implementing this interface
+     */
     @Override
-    public int compareTo(PeerFileID other) {
-        if (other.getTimestamp() < this.getTimestamp()) {
-            return -1;
-        }
-        else if (other.getTimestamp() > this.getTimestamp()) {
-            return 1;
-        }
-        else {
-            return other.getID().compareTo(this.getID());
-        }
-
+    public Supplier<? extends Header> create() {
+        return PeerFileID::new;
     }
 
+    /**
+     * Returns the size (in bytes) of the marshalled object
+     */
+    @Override
+    public int serializedSize() {
+        return Util.size(filename) + Global.BYTE_SIZE;
+    }
+
+    /**
+     * Write the entire state of the current object (including superclasses) to outstream.
+     * Note that the output stream <em>must not</em> be closed
+     *
+     * @param out
+     */
+    @Override
+    public void writeTo(DataOutput out) throws IOException {
+       // Util.write( filename,out);
+    }
+
+    /**
+     * Read the state of the current object (including superclasses) from instream
+     * Note that the input stream <em>must not</em> be closed
+     *
+     * @param in
+     */
+    @Override
+    public void readFrom(DataInput in) throws IOException, ClassNotFoundException {
+        //filename=(String)Util.read(in);
+        eof=in.readBoolean();
+
+
+    }
 }
