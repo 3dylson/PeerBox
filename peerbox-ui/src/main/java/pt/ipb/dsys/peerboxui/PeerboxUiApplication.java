3ac0a1bbd5f1e962bd2c91dc1a8e1c5601dd6e29
Edylson Manuel Gonçalves Frederico <edylson_frederico@outlook.com>
2021-06-27 23:23:29 +0100

    Init SpringBoot

---
+++ peerbox-ui/src/main/java/pt/ipb/dsys/peerboxui/PeerboxUiApplication.java
@@ -0,0 +1,13 @@
+package pt.ipb.dsys.peerboxui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeerboxUiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PeerboxUiApplication.class, args);
+    }
+
+}
---
+++ peerbox-ui/src/test/java/pt/ipb/dsys/peerboxui/PeerboxUiApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.ipb.dsys.peerboxui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeerboxUiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
