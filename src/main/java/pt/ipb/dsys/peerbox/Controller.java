9d7d8f37ba3cc380c90e2be4723496f6abd6a290
Edylson Manuel Gonçalves Frederico <edylson_frederico@outlook.com>
2021-07-05 20:50:49 +0100

    layout init with scene builder

--- src/main/java/pt/ipb/dsys/peerbox/Controller.java
+++ src/main/java/pt/ipb/dsys/peerbox/Controller.java
@@ -1,6 +1,10 @@
 package pt.ipb.dsys.peerbox;
 
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.Spinner;
+import javafx.scene.input.MouseEvent;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -8,9 +12,14 @@ import java.util.ResourceBundle;
 public class Controller implements Initializable {
 
 
+    public ListView<String> hostFiles;
+    public ListView clusterFiles;
+    public Spinner replicas;
+    public Button fetchBttn;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
     }
+
 }
