d85e191b71d9bfea13ede9e33972a12a276aa571
Edylson Manuel Gonçalves Frederico <edylson_frederico@outlook.com>
2021-07-06 16:34:29 +0100

    Bean channel connection done

--- src/main/java/pt/ipb/dsys/peerbox/configuration/AppConfig.java
+++ src/main/java/pt/ipb/dsys/peerbox/configuration/AppConfig.java
@@ -7,7 +7,9 @@ import org.springframework.context.annotation.Configuration;
 import pt.ipb.dsys.peerbox.common.PeerFile;
 import pt.ipb.dsys.peerbox.jgroups.DefaultProtocols;
 import pt.ipb.dsys.peerbox.jgroups.LoggingReceiver;
+import pt.ipb.dsys.peerbox.util.PeerUtil;
 
+import static pt.ipb.dsys.peerbox.Main.CLUSTER_NAME;
 import static pt.ipb.dsys.peerbox.Main.gossipHostname;
 
 @Configuration
@@ -16,6 +18,7 @@ public class AppConfig {
     @Bean
     @ConditionalOnMissingBean
     public JChannel channel() throws Exception {
+        PeerUtil.localhostFix(gossipHostname);
         return new JChannel(DefaultProtocols.gossipRouter(gossipHostname,12001));
     }
 
@@ -26,8 +29,13 @@ public class AppConfig {
     }
 
     @Bean
-    public PeerFile peerFile(JChannel channel, LoggingReceiver receiver) throws Exception {
-        return  new PeerFile(channel,receiver);
+    public PeerFile peerFile() throws Exception {
+        JChannel channel1 = channel();
+        LoggingReceiver receiver1 = receiver(channel1);
+        channel1.setReceiver(receiver1);
+        channel1.setDiscardOwnMessages(true);
+        channel1.connect(CLUSTER_NAME);
+        return new PeerFile(channel1,receiver1);
     }
 
 }
