1ad3b4708d8a212e2ca4c9768d17cc1aa4440dc8
Edylson Manuel Gonçalves Frederico <edylson_frederico@outlook.com>
2021-07-06 03:08:22 +0100

    approach 1 with errors

--- src/main/java/pt/ipb/dsys/peerbox/Controller.java
+++ src/main/java/pt/ipb/dsys/peerbox/Controller.java
@@ -1,16 +1,106 @@
 package pt.ipb.dsys.peerbox;
 
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.TransferMode;
+import org.springframework.stereotype.Component;
+import pt.ipb.dsys.peerbox.common.PeerBoxException;
+import pt.ipb.dsys.peerbox.common.PeerFile;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
+@Component
 public class Controller {
 
-
-    public ListView hostFiles;
-    public ListView clusterFiles;
-    public Spinner replicas;
+    @FXML
+    public ListView<String> hostFiles;
+    @FXML
+    public ListView<String> clusterFiles;
+    @FXML
+    public Spinner<Integer> replicas;
+    @FXML
     public Button fetchBttn;
+    @FXML
     public Button deleteBttn;
+
+    private PeerFile peerFile;
+
+    /*public Controller(PeerFile peerFile) {
+        this.peerFile = peerFile;
+    }*/
+
+    public void setPeerFile(PeerFile peerFile) {
+        this.peerFile = peerFile;
+    }
+
+    @FXML
+    public void initialize() throws PeerBoxException {
+        ObservableList<String> files = FXCollections.observableArrayList();
+        File[] listFiles = peerFile.readDirectory();
+        Arrays.stream(listFiles).iterator().forEachRemaining(file -> files.add(file.getName()));
+        hostFiles.setItems(files);
+
+        SpinnerValueFactory<Integer> valueFactory = replicas.getValueFactory();
+        replicas.setOnScroll(event -> {
+            if( replicas.isDisabled()) {
+                return;
+            }
+            if (!replicas.isVisible()) {
+                return;
+            }
+            double delta = event.getDeltaY();
+            if (delta == 0.0) {
+                return;
+            }
+            int increment = delta < 0 ? -1 : +1;
+            valueFactory.setValue(replicas.getValue() + increment);
+        });
+        replicas.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(1,20,2));
+        //BidirectionalBinding.bindNumber(,valueFactory.valueProperty());
+        //replicas.setEditable(true);
+
+        hostFiles.setOnDragOver(event -> {
+            if (event.getGestureSource() != hostFiles
+                    && event.getDragboard().hasFiles()) {
+                /* allow for copying. */
+                event.acceptTransferModes(TransferMode.COPY);
+            }
+            event.consume();
+        });
+
+        hostFiles.setOnDragDropped(event -> {
+            Dragboard db = event.getDragboard();
+            boolean success = false;
+            if (db.hasFiles()) {
+                //TODO Copy multiple files at once
+                File file = db.getFiles().get(0);
+                Path path = Paths.get(db.getFiles().get(0).toString());
+                try {
+                    peerFile.setData(Files.readAllBytes(path));
+                    peerFile.save(file.getName(),replicas.getValue());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                finally {
+                    files.add(file.getName());
+                    success = true;
+                }
+            }
+            event.setDropCompleted(success);
+        });
+
+    }
+
 }
+
