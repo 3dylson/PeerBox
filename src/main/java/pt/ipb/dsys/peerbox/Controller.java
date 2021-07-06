4710d2a52d40d25d4329eeaddf6e65975a1c51f2
Edylson Manuel Gonçalves Frederico <edylson_frederico@outlook.com>
2021-07-06 02:53:45 +0100

    layout complete

--- src/main/java/pt/ipb/dsys/peerbox/Controller.java
+++ src/main/java/pt/ipb/dsys/peerbox/Controller.java
@@ -1,11 +1,16 @@
 package pt.ipb.dsys.peerbox;
 
 
+import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.control.Spinner;
 
 public class Controller {
 
 
     public ListView hostFiles;
     public ListView clusterFiles;
+    public Spinner replicas;
+    public Button fetchBttn;
+    public Button deleteBttn;
 }
